3854238232f0b03292cf9818add66eab32876982
賴彥汝 <swm-tracy@laiyanrudeMacBook-Pro.local>
2021-06-04 13:12:02 +0800

    create users view table paginate & sort

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MatPaginatorIntl, MatPaginatorDefaultOptions } from '@angular/material/paginator';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private matPaginatorIntl: MatPaginatorIntl,
+  ) {
+  }
 
   // 初始化
   ngOnInit(): void {
+    // 設定分頁預設選項
+    // 設定分頁顯示資訊文字
+    this.matPaginatorIntl.itemsPerPageLabel = '每頁筆數：';
+    this.matPaginatorIntl.nextPageLabel = '下一頁';
+    this.matPaginatorIntl.previousPageLabel = '上一頁';
+    this.matPaginatorIntl.firstPageLabel = '第一頁';
+    this.matPaginatorIntl.lastPageLabel = '最終頁';
+    this.matPaginatorIntl.getRangeLabel = (page: number, pageSize: number, length: number): string => {
+      if (length === 0 || pageSize === 0) {
+        return `第 0 筆`;
+      }
+
+      length = Math.max(length, 0);
+      const startIndex = page * pageSize;
+      const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+
+      return `第 ${startIndex + 1} - ${endIndex} 筆`;
+    };
   }
 
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
-import { MatPaginator } from '@angular/material/paginator';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator,MatPaginatorIntl } from '@angular/material/paginator';
 
 // model
 import { User } from '../models/user';
@@ -14,21 +16,31 @@ import { Users } from '../services/mock/users';
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, AfterViewInit {
   dataSource = new MatTableDataSource<User>();
   displayedColumns = ['select', 'icons', 'Name', 'Phone', 'Mail'];
 
-  constructor() { }
+  filter$ = new Subject<string>();
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+
+  constructor(private matPaginatorIntl: MatPaginatorIntl) {
+  }
 
   ngOnInit(): void {
-
     // users
     this.dataSource.data = Users;
+    // filter
+    this.filter$.pipe(
+      debounceTime(300)
+    ).subscribe(p => {
+      this.dataSource.filter = p;
+    });
   }
 
   ngAfterViewInit() {
-    console.log('ngAfterViewInit');
-    console.log(this.dataSource);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
 }
